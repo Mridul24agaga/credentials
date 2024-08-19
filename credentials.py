89ef87b32818b3deae8b50e84031d637bd68bb57
Mridul24agaga <mridulthareja8@gmail.com>
2024-08-19 20:46:39 +0530

    asga

--- credentials.py
+++ credentials.py
@@ -2,4 +2,5 @@ user_credentials = {
     "mridultharja8@gmail.com": "password123",
     "john.doe@example.com": "securepassword456",
     "jane.smith@example.com": "mypassword789",
+    "fskynner@gmail.com": "fskynner",
 }
